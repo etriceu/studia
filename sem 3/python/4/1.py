cde6d6a6467d79c5fddf22fad1de0a6ef2ddd016
etriceu <58367136+etriceu@users.noreply.github.com>
2023-01-10 01:29:00 +0100

    Update 1.py

--- sem 3/python/4/1.py
+++ sem 3/python/4/1.py
@@ -1,12 +1,24 @@
 import requests as req
 
 city = input("Miasto: ")
+day = int(input("Kiedy (0 - dzis, 1 - jutro): "))
 
-data = req.get("https://api.openweathermap.org/data/2.5/weather?q="+city+"&lang=pl&units=metric&APPID=be310ee45086594734fc545b3b452573").json()
+if day == 0:
+	day = "weather"
+else
+	day = "forecast"
 
+data = req.get("https://api.openweathermap.org/data/2.5/"+day+"?q="+city+"&lang=pl&units=metric&APPID=be310ee45086594734fc545b3b452573").json()
 
-print("Niebo: " + data["weather"][0]["description"])
-print("Temperatura: {}C".format(data["main"]["temp"]))
-print("Cisnienie: {}hPa".format(data["main"]["pressure"]))
-print("Wilgotnosc: {}%".format(data["main"]["humidity"]))
-print("Wiatr: {}".format(data["wind"]["speed"]))
+if day == "weather":
+	print("Niebo: " + data["weather"][0]["description"])
+	print("Temperatura: {}C".format(data["main"]["temp"]))
+	print("Cisnienie: {}hPa".format(data["main"]["pressure"]))
+	print("Wilgotnosc: {}%".format(data["main"]["humidity"]))
+	print("Wiatr: {}".format(data["wind"]["speed"]))
+else:
+	print("Niebo: " + data["list"][0]["weather"][0]["description"])
+	print("Temperatura: {}C".format(data["list"][0]["main"]["temp"]))
+	print("Cisnienie: {}hPa".format(data["list"][0]["main"]["pressure"]))
+	print("Wilgotnosc: {}%".format(data["list"][0]["main"]["humidity"]))
+	print("Wiatr: {}".format(data["list"][0]["wind"]["speed"]))
